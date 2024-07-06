50f047ea17004c7192eb4d289f19ea4422a95e1e
Goldyn19 <nwaezegolden2002@gmail.com>
2024-07-06 07:24:02 +0100

    ready to host

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,44 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { BsFillCartFill, BsFillPersonFill } from "react-icons/bs";
 
 const NavBar = () => {
+  const Links = [
+    { name: "HOME", link: "/" },
+    { name: "ABOUT", link: "/about" },
+    { name: "PRODUCTS", link: "/product" },
+    { name: "CONTACT", link: "/" },
+  ];
+  const [open, setOpen] = useState(false);
+
   return (
+    <div className="shadow-md w-full top-0 left-0 bg-white z-50">
+      <div className="container flex items-center justify-between mx-auto py-4 md:px-10 px-7">
+        <div className="cursor-pointer flex items-center">
+          <span className="text-[38px] h-[70px] font-[600] mb-[-12px] text-[#4CAF50]">
+            Dyne
+          </span>
+        </div>
 
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-5 cursor-pointer lg:hidden"
+        >
+          {open ? (
+            <IoClose className="text-4xl text-[#bcbcbc]" />
+          ) : (
+            <IoMenu className="text-4xl text-[#bcbcbc]" />
+          )}
+        </div>
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-  </a>
-  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-      <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <a href="/" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
-      </li>
-      <li>
-        <a href="/" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-      </li>
-      <li>
-        <a href="/" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-      </li>
-      <li>
-        <a href="/" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
+        <ul
+          className={`lg:flex lg:items-center lg:pb-0  absolute lg:static text-[20px] font-[300] bg-white lg:z-auto z-50 left-0 w-full lg:w-auto lg:pl-0 pl-6 transition-all duration-500 ease-in ${
+            open ? "top-20" : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="lg:mx-8 text-xl lg:my-0  my-3">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
 
-  )
-}
+        <ul
+          className={`flex lg:items-center lg:pb-0 justify-end pr-7 lg:pr-0 absolute lg:static text-[20px] font-[500] bg-white lg:z-auto z-50 left-0 w-full lg:w-auto lg:pl-0 pl-2 transition-all duration-500 ease-in ${
+            open ? "top-[250px] py-3 space-x-4" : "top-[-490px]"
+          }`}
+        >
+          <li className="block relative lg:mx-3 ">
+            <BsFillCartFill className="text-2xl text-gray-800" />
+            <span className="absolute top-0 left-full transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 text-gray-800 rounded-full px-2 py-1 text-xs">
+              7
+            </span>
+          </li>
+          <li className="block relative lg:mx-3">
+            <BsFillPersonFill className="text-2xl text-gray-800" />
+            <span className="absolute top-0 left-full transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 text-gray-800 rounded-full px-2 py-1 text-xs">
+              99+
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
